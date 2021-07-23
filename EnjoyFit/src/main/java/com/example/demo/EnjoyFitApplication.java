04dd536b3f7ad9d4fa94a96d509a4502629b91a4
Tommolo <72791771+Tommolo@users.noreply.github.com>
2021-07-23 10:12:46 +0200

    PrimoCommit

---
+++ EnjoyFit/src/main/java/com/example/demo/EnjoyFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyFitApplication.class, args);
+	}
+
+}
